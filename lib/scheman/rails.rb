6d4b5f86c1a48dcf8adc20040601e33ad3ae26c8
Ryo Nakamura <r7kamura@gmail.com>
2014-07-15 16:19:38 +0900

    Improve rake db:scheman:diff command

--- lib/scheman/rails.rb
+++ lib/scheman/rails.rb
@@ -1,3 +1,6 @@
+require "open3"
+require "scheman"
+
 require "scheman/rails/dumper"
 require "scheman/rails/railtie"
 require "scheman/rails/version"
--- lib/scheman/rails/dumper.rb
+++ lib/scheman/rails/dumper.rb
@@ -1,7 +1,7 @@
 module Scheman
   module Rails
     class Dumper
-      COMMAND = "mysqldump"
+      SCHEMA_PATH = "db/schema.sql"
 
       def self.call(*args)
         new(*args).call
@@ -14,16 +14,79 @@ module Scheman
       end
 
       def call
-        system(COMMAND, *options)
+        print statements.join("\n\n").rstrip + "\n"
       end
 
       private
 
-      def options
-        default_options + additional_options
+      def statements
+        result = []
+        result << create_database_statement if has_unknown_database_error?
+        result << diff
+        result
       end
 
-      def default_options
+      def create_database_statement
+        "CREATE DATABASE `#{database}`;"
+      end
+
+      def diff
+        Scheman::Diff.new(
+          before: before_schema,
+          after: after_schema,
+          type: schema_type,
+        )
+      end
+
+      def dump
+        @dump ||= Open3.capture3(command)
+      end
+
+      def dump_result
+        dump[0]
+      end
+
+      def has_dump_error?
+        dump[2] != 0
+      end
+
+      def dump_error_message
+        dump[1]
+      end
+
+      def has_unknown_database_error?
+        has_dump_error? && /Unknown database/ === dump_error_message
+      end
+
+      def before_schema
+        if has_unknown_database_error?
+          ""
+        else
+          dump
+        end
+      end
+
+      def after_schema
+        if after_schema_pathname.exist?
+          after_schema_pathname.read
+        else
+          ""
+        end
+      end
+
+      def after_schema_pathname
+        @after_schema_path ||= ::Rails.root.join(SCHEMA_PATH)
+      end
+
+      def command
+        "mysqldump " + command_options.join(" ")
+      end
+
+      def command_options
+        default_command_options + additional_command_options
+      end
+
+      def default_command_options
         [
           database,
           "--compact",
@@ -31,7 +94,7 @@ module Scheman
         ]
       end
 
-      def additional_options
+      def additional_command_options
         result = []
         result.concat(["--default-character-set", encoding]) if encoding
         result.concat(["--host", host]) if host
@@ -72,6 +135,15 @@ module Scheman
 
       def username
         @configuration["username"]
+      end
+
+      def schema_type
+        case @configuration["adapter"]
+        when "mysql2"
+          "mysql"
+        else
+          raise
+        end
       end
     end
   end
