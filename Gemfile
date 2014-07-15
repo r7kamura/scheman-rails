8ffbf89b459db7e45c9f7015ffcfeccfbeaba0a1
Ryo Nakamura <r7kamura@gmail.com>
2014-07-15 14:25:27 +0900

    bundle gem scheman-rails

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in scheman-rails.gemspec
+gemspec
---
+++ lib/scheman/rails.rb
@@ -0,0 +1,7 @@
+require "scheman/rails/version"
+
+module Scheman
+  module Rails
+    # Your code goes here...
+  end
+end
